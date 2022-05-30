80b5f1a46b1f6faa9d1d6e8404aea0e1f43812c7
yasmineexia <yasmine.benoudina.dz@viacesi.fr>
2022-05-30 11:08:06 +0000

    modifs

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,16 +1,16 @@
 import Api from "./api";
 
 export default {
-	login(creditentials) {
-        return Api.post("/login", creditentials);
-	},
-	checklogin() {
-		return Api.get("/checklogin", { });
-	},
-	getAutorisation(query) {
-		return Api.get("/fonction", query);
-	},
-	getAutorisations(query) {
-		return Api.get("/autorisation", query);
-	}
+  login(creditentials) {
+    return Api.post("/login", { ...creditentials, type: "admin" });
+  },
+  checklogin() {
+    return Api.get("/checklogin", {});
+  },
+  getAutorisation(query) {
+    return Api.get("/fonction", query);
+  },
+  getAutorisations(query) {
+    return Api.get("/autorisation", query);
+  },
 };
---
+++ src/views/client/ClientShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex flex-col w-full h-screen bg-gray-700 overflow-hidden space-y-1"
+  >
+    <div
+      class="flex self-start w-full items-center space-x-3 h-16 px-16 md:items-center"
+    >
+      <div class="flex card lg:card-side bg-base-100 shadow-xl w-full md:mt-96">
+        <div class="card-body">
+          <h2 class="card-title">Profile utilisateur</h2>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
