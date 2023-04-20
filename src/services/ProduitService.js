0e638e008daa5f97325236f3de295b04cb280ba2
nazzim29 <kadouche.n@gmail.com>
2023-04-21 00:21:27 +0100

    fix image upload

--- src/services/ProduitService.js
+++ src/services/ProduitService.js
@@ -4,25 +4,30 @@ export function get(query) {
 	return Api.get("/produit", { ...query });
 }
 export function getCouleurs(query) {
-    return Api.get("/couleur", { ...query });
+	return Api.get("/couleur", { ...query });
 }
 export function getTypes(query) {
-    return Api.get("/typeProduit", { ...query });
-}	
+	return Api.get("/typeProduit", { ...query });
+}
 export function show(id) {
 	return Api.get("/produit/" + id);
 }
 export function create(produit) {
 	const data = new FormData();
 	for (let key in produit) {
-		// if (typeof produit[key] === "object") {
-		// 	data.append(key, JSON.stringify(produit[key]));
-		// 	console.log(JSON.stringify(produit[key]));
-		// 	continue;
-		// }
+		if (typeof produit[key] === "object" && key === "image") {
+			const imageFile = produit[key];
+			data.append("image", imageFile, imageFile.name);
+			continue;
+		}
+		if (typeof produit[key] === "object") {
+			data.append(key, JSON.stringify(produit[key]));
+			console.log(JSON.stringify(produit[key]));
+			continue;
+		}
 		data.append(key, produit[key]);
 	}
-	return Api.post("/produit",data , { ContentType: "multipart/form-data" });
+	return Api.post("/produit", data, { ContentType: "multipart/form-data" });
 }
 export function destroy(id) {
 	return Api.delete(`/produit/${id}`);
@@ -38,7 +43,7 @@ export function update(produit) {
 		// }
 		data.append(key, produit[key]);
 	}
-	console.log(data.values())
+	console.log(data.values());
 	return Api.post(`/produit/${produit.id}`, data, {
 		ContentType: "multipart/form-data",
 	});
@@ -47,7 +52,9 @@ export function addFormule(formule) {
 	return Api.post(`/produit/${formule.ProduitId}/formule`, formule);
 }
 export function removeFormule(formule) {
-	return Api.delete(`/produit/${formule.ProduitId}/formule/${formule.MatiereId}`);
+	return Api.delete(
+		`/produit/${formule.ProduitId}/formule/${formule.MatiereId}`
+	);
 }
 export default {
 	get,
